79f2916762616f896c04b9b0b5299f022a8782bc
richard miruka onsare <richardonsare15@gmail.com>
2023-06-22 14:11:34 +0300

    couldn't meet the third objective

--- server/app.py
+++ server/app.py
@@ -12,7 +12,31 @@ def index():      #view returns a simple string of HTML
 @app.route('/print/<string:param>') # <string:param> is a URL parameter that represents the string to be printed
 def print_string(param):           #The print_string function takes the param parameter, prints it in the console using print(param),
     print(param)
-    return param                   #and returns the parameter as the response to display it in the web browser.
+    return param  #and returns the parameter as the response to display it in the web browser.
 
+#In this code, a new route /count/<int:param> is defined
+@app.route('/count/<int:param>')                      #<int:param> is a URL parameter that represents the integer parameter for the count view.
+def count(param):                                     #The function takes the param parameter, generates a range of numbers from 0 to param, and joins them together with newline characters ('\n')
+    numbers = '\n'.join(str(i) for i in range(param))
+    numbers += '\n'                                   # Add trailing newline character
+    return numbers                                    #The resulting string with numbers on separate lines is returned as the response.
+
+@app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    if operation=='+':
+        result = num1 + num2
+    elif operation=='-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+            result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2  
+    else:
+        return 'Invalid operation'
+    
+    return str(result)
+                 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
