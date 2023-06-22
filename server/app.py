42bd38029fa224c3dd55982a7b31580c9138f4c6
richard miruka onsare <richardonsare15@gmail.com>
2023-06-22 12:29:58 +0300

    A print_string view has taken one parameter, a string. It  print the string in the console and display it in the web browser

--- server/app.py
+++ server/app.py
@@ -4,11 +4,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/')   # defined a route for the root URL in my Flask application
+def index():      #view returns a simple string of HTML
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-
+#In this code, a new route '/print/<string:param>' is defined
+@app.route('/print/<string:param>') # <string:param> is a URL parameter that represents the string to be printed
+def print_string(param):           #The print_string function takes the param parameter, prints it in the console using print(param),
+    print(param)
+    return param                   #and returns the parameter as the response to display it in the web browser.
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
